865c4e06052059ca9642d491a33aee43ae90b8aa
Yxc <289174785@qq.com>
2017-05-22 19:25:30 +0800

    index施工

---
+++ src/lib/config.js
@@ -0,0 +1,9 @@
+require.config({
+	// baseUrl:'lib'//data-main文件所在的文件夹
+	paths : {
+		//这里的路径基于baseUrl
+        "jquery": "jquery-3.1.1",
+        "mobile": "./../js/mobile_js"       
+    },
+
+});
---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+require(['config'],function(){
+	require(['jquery','mobile'],function($,m){
+		m.mobile();
+	})
+})
