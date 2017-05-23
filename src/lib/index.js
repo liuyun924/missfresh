cc9b4cb8ec716c3338e2d2f6d1c945ef109f55f9
Yxc <289174785@qq.com>
2017-05-23 11:26:50 +0800

    index购物车效果完成

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,9 +3,39 @@ require(['config'],function(){
 		//移动端适配
 		m.mobile();
 
-		$('.s_car').on('click',function(){
-			$(this).css('display','none');
-			$(this).next().css('display','block');
+		//购物车按钮效果
+		$('.s_car').each(function(i){
+			$('.s_car').eq(i).on('click',function(){
+				$(this).css('display','none');
+				$(this).next().css('display','block');
+			});
+		});
+
+		//购物车删减效果
+		$('.car_remove').each(function(i){
+			$('.car_remove').eq(i).on('click',function(){
+				var val = parseInt($('.car_num')[i].innerText);
+				if(val == 1){
+					$(this).parent().css('display','none');
+					$(this).parent().prev().css('display','block');
+					return;
+				}else{
+					$('.car_num')[i].innerText = val - 1;
+				}
+
+			});
 		})
+
+		//购物车添加效果
+		$('.car_add').each(function(i){
+			
+			$('.car_add').eq(i).on('click',function(){
+
+				var val = parseInt($('.car_num')[i].innerText);
+
+				$('.car_num')[i].innerText = val + 1; 
+			})
+		});
+		
 	})
 });
