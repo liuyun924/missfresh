fc9f71beda5d445278167414e7494f2a0d719fed
maskhb <734387564@qq.com>
2017-05-24 18:05:40 +0800

    部分详情页

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,73 @@
-﻿//引入gulp模块
-//commonjs规范引用模块
+﻿// 引入gulp模块
+// commonjs规范引用模块
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
 //这里创建gulp任务
 //用来编译sass文件
-//创建一个任务叫compileSass，在里面编译sass文件
-gulp.task('compileSass',function(){
-	// 先查找sass文件的位置
-	gulp.src('src/sass/*.scss')
+gulp.task('compileSass', function() {
+    //先查找sass文件所在的位置
+    gulp.src('src/sass/*.scss')
 
-	//通过pipe方法导入到gulp插件中，实现编译
-	.pipe(sass({outputStyle:'compact'}).on('error',sass.logError))
-	//把编译后的文件输入到src/css
-	.pipe(gulp.dest('src/css'));
+    // 通过pipe 方法导入到 gulp 的插件中实现编译sass
+    .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+
+    // 把编译后的文件输出
+    .pipe(gulp.dest('src/css'));
 });
 
-//监听文件修改，执行相对任务
-gulp.task('jtSass',function(){
-	gulp.watch('src/sass/*.scss',['compileSass']);
-})
+// 监听文件修改，执行相应任务
+gulp.task('jtSass', function() {
+    // 监听sass文件，如果有修改，则编译
+    gulp.watch('src/sass/*.scss', ['compileSass']);
+});
+
+
+// // 用于js文件的压缩
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
+// var concat = require('gulp-concat');
+// gulp.task('compressJS',function(){
+// 	gulp.src('src/js/*.js')
+
+// 	// 合并
+// 	.pipe(concat('page.js'))
+
+// 	// 输入合并后但未压缩的版本
+// 	.pipe(gulp.dest('dist/js/'))
+
+// 	// 压缩
+// 	.pipe(uglify({
+// 		mangle:false,
+// 		compress:false
+// 	}))
+
+// 	// 重命名
+// 	.pipe(rename({
+// 		suffix:'.min'
+// 	}))
+
+// 	// 输出
+// 	.pipe(gulp.dest('dist/js/'))
+// });
+
+// 同步任务
+var browserSync = require('browser-sync');
+gulp.task('server', function() {
+    browserSync({
+        server: "./src",
+
+        // 代理服务器
+        // proxy: 'http://localhost/project/',
+
+        // 自定义端口
+        // port: 8080,
+
+        // 监听文件修改，自动刷新浏览器
+        files: ['./src/html/*.html', './src/css/*.css', './src/js/*.js']
+    });
+
+    // 监听sass文件修改，执行编译sass文件
+    gulp.watch('src/sass/*.scss', ['compileSass']);
+});
