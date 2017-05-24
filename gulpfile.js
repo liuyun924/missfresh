ec6873d298d129e3af03bd70490ec7d030ebc705
liangsansan <375185682@qq.com>
2017-05-24 09:02:47 +0800

    lrs:23号 静态页面

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,15 @@ var sass = require('gulp-sass');
 //创建一个任务叫compileSass，在里面编译sass文件
 gulp.task('compileSass',function(){
 	// 先查找sass文件的位置
-	gulp.src('sass/*.scss')
+	gulp.src('src/sass/*.scss')
 
 	//通过pipe方法导入到gulp插件中，实现编译
 	.pipe(sass({outputStyle:'compact'}).on('error',sass.logError))
 	//把编译后的文件输入到src/css
-	.pipe(gulp.dest('css'));
+	.pipe(gulp.dest('src/css'));
 });
 
 //监听文件修改，执行相对任务
 gulp.task('jtSass',function(){
-	gulp.watch('sass/*.scss',['compileSass']);
+	gulp.watch('src/sass/*.scss',['compileSass']);
 })
